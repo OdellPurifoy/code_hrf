e75e38713d5fb54784fcc0dd71edf377d2ecb97b
Odell Purifoy <opurifoy@springhealth.com>
2023-07-28 21:13:26 -0400

    Ran rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_29_011000) do
+ActiveRecord::Schema[7.0].define(version: 20_230_729_011_000) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pgcrypto"
-  enable_extension "plpgsql"
-
+  enable_extension 'pgcrypto'
+  enable_extension 'plpgsql'
 end
