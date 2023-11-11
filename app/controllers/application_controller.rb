95319587125bbc91b94cb2e7cf720b07bef61450
Odell Purifoy <opurifoy@springhealth.com>
2023-11-11 15:42:27 -0500

    Updated edit profile form and account update controller bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name date_of_birth phone_number])
-    devise_parameter_sanitizer.permit(:account_updated,
+    devise_parameter_sanitizer.permit(:account_update,
                                       keys: %i[first_name last_name date_of_birth phone_number])
   end
 end
